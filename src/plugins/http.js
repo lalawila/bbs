f11b0b1cf4ad90179a65af5b0746e85744ed1050
lalawila <wangosky@gmail.com>
2022-07-07 04:48:13 +0800

    修复携带 token 的 bug

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -3,21 +3,42 @@ import { useUserStore } from "../stores/user"
 
 export default {
     install: (app) => {
-        const userStore = useUserStore()
-        const config = {
+        // 创建实例时配置默认值
+        const http = axios.create({
             baseURL: "https://3yya.com/u/d8cf630cf5f367cc/bbs/app",
-        }
+        })
 
-        if (userStore.isLogged) {
-            // 只要已经登录
-            // 就会配置 token
-            config["headers"] = {
-                Authorization: userStore.token,
+        const userStore = useUserStore()
+
+        // 请求前拦截
+        http.interceptors.request.use(function (config) {
+            if (userStore.isLogged) {
+                config.headers["Authorization"] = userStore.token
             }
-        }
+            return config
+        })
 
-        const http = axios.create(config)
+        // 响应后拦截
+        http.interceptors.response.use(
+            function (response) {
+                // 成功时执行
+                // 2xx 的状态码
+                return response
+            },
+            function (error) {
+                // 失败时执行
+                // 包括超时，网络错误，所有非 2xx 的状态码
+                if (error.response.status === 400) {
+                    if (error.response.data.code === 1100) {
+                        userStore.logout()
+                    }
+                }
 
+                return Promise.reject(error)
+            }
+        )
+
+        // 将 axios 绑定到全局属性中
         app.config.globalProperties.$http = http
     },
 }
